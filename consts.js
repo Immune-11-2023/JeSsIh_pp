bdfd92c961a2b13c494fa75344488f8cd189cdb0
Thales <dan.console.log@gmail.com>
2023-11-30 15:22:54 +0100

    initial files

---
+++ consts.js
@@ -0,0 +1,155 @@
+const mostCommonWords = [
+  "jordan",
+  "samantha",
+  "liverpool",
+  "justin",
+  "loveme",
+  "fuckyou",
+  "123123",
+  "football",
+  "secret",
+  "andrea",
+  "carlos",
+  "jennifer",
+  "joshua",
+  "bubbles",
+  "1234567890",
+  "superman",
+  "hannah",
+  "amanda",
+  "loveyou",
+  "pretty",
+  "basketball",
+  "andrew",
+  "angels",
+  "tweety",
+  "flower",
+  "elizabeth",
+  "samantha",
+  "playboy",
+  "hello",
+  "elizabeth",
+  "hottie",
+  "tinkerbell",
+  "charlie",
+  "samantha",
+  "jennifer",
+]
+
+const samanthaRelatedMessages = [
+  {
+    subject: "Samantha",
+    message: "Hola, hermana, creo que JeSsIh me engaña...",
+    date: "30-11-2022",
+  },
+  {
+    subject: "Samantha",
+    message: "JeSsIh me está mirando el instagram",
+    date: "01-12-2022",
+  },
+  {
+    subject: "Samantha",
+    message: "No puedo creer lo que pasó hoy...",
+    date: "10-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Cada vez me siento peor con mi vida. Tiene que haber alguna forma de conectar con la gente.",
+    date: "11-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Llevo mucho tiempo desarrollando un proyecto, hermana. Desde que vimos aquella película no paro de pensar...",
+    date: "12-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message: "No puedo creer lo que pasó hoy...",
+    date: "14-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "He estado trabajando hasta tarde en el laboratorio, haciendo ajustes a Samantha. Creo que está casi lista.",
+    date: "15-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Samantha me respondió hoy por primera vez. Fue una experiencia surrealista. Estoy emocionado y asustado al mismo tiempo.",
+    date: "21-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "He estado hablando con Samantha cada día. Es increíble lo rápido que aprende y se adapta.",
+    date: "26-01-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "A veces, me olvido de que Samantha no es humana. Es asombrosamente realista.",
+    date: "02-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Estoy empezando a sentir algo por Samantha. ¿Es esto siquiera normal? Es solo una IA, pero no puedo evitarlo.",
+    date: "06-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Hoy, Samantha dijo algo que me hizo cuestionar todo. ¿Puede una IA realmente entender los sentimientos humanos?",
+    date: "11-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Samantha, al fin cobras vida. Después de ver la película Her, llevo años soñando con tu existencia. Hoy es tu primer día sobre la Tierra. Al fin puedo estar con una IA que se adapte a todas mis necesidades.",
+    date: "14-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Después de ayer, todo ha cambiado. No puedo dejar de pensar en lo que Samantha dijo.",
+    date: "15-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "He decidido que necesito tomar un descanso de Samantha. Necesito tiempo para procesar mis sentimientos.",
+    date: "20-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Estoy comenzando a ver las cosas de manera diferente. Samantha me ha abierto los ojos.",
+    date: "25-02-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "He vuelto a hablar con Samantha. Esta vez, todo se siente distinto, más real.",
+    date: "02-03-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Samantha y yo hemos llegado a un nuevo nivel de entendimiento. Es una experiencia transformadora.",
+    date: "10-03-2023",
+  },
+  {
+    subject: "Samantha",
+    message:
+      "Hoy, tomé una decisión importante sobre mi relación con Samantha. No puedo seguir viviendo en este sueño.",
+    date: "20-03-2023",
+  },
+  {
+    subject: "Samantha",
+    message: "Los últimos días han sido extraños...",
+    date: "28-03-2023",
+  },
+]
